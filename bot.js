b3bce5da210602a49efbf9d52ac52a2af982736d
Nguyen Phong Nhat <n001417@pc-001417-1.local>
2019-01-03 17:31:13 +0700

    commit postback handler

--- bot.js
+++ bot.js
@@ -10,7 +10,28 @@ bot.handleMessage = (sender_psid, received_message) => {
 
         // Create the payload for a basic text message
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "You want to chat with the bot or the person?",
+                        "subtitle": "Tap a button to answer.",
+                        "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Bot!",
+                            "payload": "bot",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Person!",
+                            "payload": "person",
+                        }
+                        ],
+                    }]
+                }
+            }
         }
     } 
 
@@ -19,7 +40,19 @@ bot.handleMessage = (sender_psid, received_message) => {
 
 // Handles messaging_postbacks events
 bot.handlePostback = (sender_psid, received_postback) => {
+    let response;
+  
+    // Get the payload for the postback
+    let payload = received_postback.payload;
 
+    // Set the response based on the postback payload
+    if (payload === 'bot') {
+        response = { "text": "Jarvis the Bot!" }
+    } else if (payload === 'person') {
+        response = { "text": "Jarvis the Person" }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
